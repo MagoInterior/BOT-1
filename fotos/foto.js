5afe26dee58e2e59d3e5debf8ab283fb5fe4090c
Carlos20o <carlosnatanaeln2007@gmail.com>
2023-02-24 15:29:21 -0500

    first commit

---
+++ fotos/foto.js
@@ -0,0 +1,17 @@
+[
+{"result": "https://telegra.ph/file/deb2429ad5124b8947d61.jpg"},
+{"result": "https://telegra.ph/file/11cc80b4ffb32a8976de0.jpg"},
+{"result": "https://telegra.ph/file/cae36080c0ed37b18df7d.jpg"},
+{"result": "https://telegra.ph/file/77cdaa4d3aef9d4b8f997.jpg"},
+{"result": "https://telegra.ph/file/fda20bd907f2e65924f70.jpg"},
+{"result": "https://telegra.ph/file/bda4767dd12b7cb3f6468.jpg"},
+{"result": "https://telegra.ph/file/49397a899928b5784ee80.jpg"},
+{"result": "https://telegra.ph/file/0f6ec47d92da3d5cce0fd.jpg"},
+{"result": "https://telegra.ph/file/3d38091ea2a2bf419b569.jpg"},
+{"result": "https://telegra.ph/file/2557a9d006d53b70050bb.jpg"},
+{"result": "https://telegra.ph/file/aabf309811706df4023ad.jpg"},
+{"result": "https://telegra.ph/file/00dfa7b19bc172a881a26.jpg"},
+{"result": "https://telegra.ph/file/ac44339c35557b75d9f9a.jpg"},
+{"result": "https://telegra.ph/file/ec11a4545d1ae5799801b.jpg"},
+{"result": "https://telegra.ph/file/8ad27424140c83cc5798d.jpg"}
+]
